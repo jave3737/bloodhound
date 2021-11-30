319736fb153b9880e9c004fe17559e6d4b8303c0
Alejandro Miranda <alejandro.miranda@hey.com>
2021-11-30 16:19:01 -0500

    Parsing simple command using clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::fs;
+use clap::{App, Arg};
+const CONFIG_FILE: &str = "config.yaml";
 fn main() {
-    println!("Hello, world!");
+
+    let matches = App::new("Bloodhound")
+        .version("1.0")
+        .author("Alejandro Miranda <alejandro.miranda@hey.com>")
+        .about("Dig through pinboard bookmarks")
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .takes_value(true)
+                .help("Create a config.yaml that stores your api token")
+        )
+        .get_matches();
+
+    if let Some(res) = matches.value_of("config") {
+        println!("Creating config file");
+        if let Err(err) = std::fs::File::create(CONFIG_FILE) {
+            eprintln!("Error creating {} due to {}",CONFIG_FILE, err);
+        }
+    } 
 }
