1712722d1a257e564b33a6ca0b47590efca6ec5d
Alejandro Miranda <alejandro.miranda@hey.com>
2021-11-30 21:32:56 -0500

    Added the option to create a config.yaml, which the token will be used for pinboard access

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::fs;
-use clap::{App, Arg};
+use std::io::Write;
+use clap::{App,Arg};
+use std::collections::BTreeMap;
+
 const CONFIG_FILE: &str = "config.yaml";
+
 fn main() {
 
     let matches = App::new("Bloodhound")
@@ -17,9 +20,21 @@ fn main() {
         .get_matches();
 
     if let Some(res) = matches.value_of("config") {
-        println!("Creating config file");
-        if let Err(err) = std::fs::File::create(CONFIG_FILE) {
-            eprintln!("Error creating {} due to {}",CONFIG_FILE, err);
+        println!("Creating/Overwriting {}", CONFIG_FILE);
+        match std::fs::File::create(CONFIG_FILE)
+        {
+            Ok(mut o) => {
+                let mut map = BTreeMap::new();
+                map.insert("api".to_string(), res);
+                let serial_map = serde_yaml::to_string(&map).expect("Error");
+                o.write_all(serial_map.as_bytes()).expect("Error");
+            }
+            Err(e) => {
+                eprintln!("Error creating {} due to {}",CONFIG_FILE, e);
+            }
         }
-    } 
+        }
+    if !std::path::Path::new(CONFIG_FILE).exists() {
+        eprintln!("No {} found! You must create it first using the config option",CONFIG_FILE);
+    }
 }
