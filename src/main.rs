13c11c347be8d1b45b04b2f4215dd71706971bfa
Alejandro Miranda <jave3737@pm.me>
2021-12-14 23:43:50 -0500

    Reformatting

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ extern crate log;
 use clap::{App, Arg};
 use log::Level::Info;
 use log::{info, log_enabled};
+use reqwest::blocking::Client;
+use reqwest::header::USER_AGENT;
 use reqwest::Request;
+use reqwest::{Method, Url};
 use std::collections::BTreeMap;
 use std::collections::HashMap;
 use std::env;
 use std::io::Write;
-use reqwest::blocking::Client;
-use reqwest::header::USER_AGENT;
-use reqwest::{Url,Method};
 
 const CONFIG_FILE: &str = "config.yaml";
 const PINBOARD_URL: &str = "https://api.pinboard.in/v1/tags/get";
@@ -29,15 +29,15 @@ fn parse_option(map: &mut HashMap<String, String>, matches: clap::ArgMatches) {
 
 fn create_config_file(map: HashMap<String, String>) -> Result<bool, anyhow::Error> {
     let mut bt_map = BTreeMap::new();
-     bt_map.insert(ToString::to_string(&"api"), map.contains_key("config"));
-     let mut file = std::fs::File::create(CONFIG_FILE)?;
-     let serial_map = serde_yaml::to_string(&bt_map)?;
-     file.write_all(serial_map.as_bytes())?;
+    bt_map.insert(ToString::to_string(&"api"), map.contains_key("config"));
+    let mut file = std::fs::File::create(CONFIG_FILE)?;
+    let serial_map = serde_yaml::to_string(&bt_map)?;
+    file.write_all(serial_map.as_bytes())?;
     Ok(true)
 }
 
 fn use_env_var() -> (bool, String) {
-    let status:bool;
+    let status: bool;
     let env_api_token = env::var("PINBOARD_API").unwrap_or("none".to_string());
 
     if env_api_token.to_owned().eq("none") {
@@ -46,8 +46,11 @@ fn use_env_var() -> (bool, String) {
         status = true;
     }
 
-    if log_enabled!(Info) && status == true{
-        info!("Found api token via environment variable: {}", env_api_token);
+    if log_enabled!(Info) && status == true {
+        info!(
+            "Found api token via environment variable: {}",
+            env_api_token
+        );
         info!("Will NOT be using api token specified in {}", CONFIG_FILE);
     }
 
@@ -57,7 +60,7 @@ fn use_env_var() -> (bool, String) {
 fn main() {
     env_logger::init();
 
-    let  matches = App::new("Bloodhound")
+    let matches = App::new("Bloodhound")
         .version("1.0")
         .author("Alejandro Miranda <alejandro.miranda@hey.com>")
         .about("Dig through pinboard bookmarks")
@@ -77,7 +80,7 @@ fn main() {
     let (_env_status, env_string) = use_env_var();
 
     //create the file if env_status = false, config doenst exists, and the create config option was set
-    //create_config_file(map).unwrap();    
+    //create_config_file(map).unwrap();
 
     let client = Client::new();
     let url = reqwest::Url::parse(PINBOARD_URL).unwrap();
@@ -86,6 +89,4 @@ fn main() {
     println!("{:?}", &req);
     let res = req.send().unwrap();
     println!("{:?}", &res.status());
-
-
 }
